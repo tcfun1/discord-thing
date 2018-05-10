e9419561773707cfa6c0fa9f325aae3e409eff7d
tcfun1 <38513042+tcfun1@users.noreply.github.com>
2018-05-10 09:57:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,24 @@ client.on("message", (message) => {
     message.channel.send("pong!");
   }
 });
-
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
 client.login("NDM4Mzc1NDgyNjgxOTgyOTg3.DcDshQ.qBf2z6GyZI85wnqNDzlDlol6duU");
