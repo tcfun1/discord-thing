2dde5bf899cd3c256fd94ca4673ad5f21fd00c05
tcfun1 <38513042+tcfun1@users.noreply.github.com>
2018-05-27 09:12:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-// require the discord.js module
-const Discord = require('discord.js');
-
-// create a new Discord client
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-// when the client is ready, run this code
-// this event will trigger whenever your bot:
-// - finishes logging in
-// - reconnects after disconnecting
-client.on('ready', () => {
-    if (message.content === '!ping') {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Pong.');
-}
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
 // login to Discord with your app's token
 client.login('MrwHMF_ibVQK8lF2AnZtvszRBy5ohAXM');
