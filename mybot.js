15d5e563759078011ee86f981922e7a29a1d0b97
tcfun1 <38513042+tcfun1@users.noreply.github.com>
2018-07-22 14:04:39 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -34,6 +34,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
             // Just add any case commands if you want to..
+                
+    static void UpdatePresence()
+    {
+        DiscordRichPresence discordPresence;
+        memset(&discordPresence, 0, sizeof(discordPresence));
+        discordPresence.state = "Playing Solo";
+        discordPresence.details = "Competitive";
+        discordPresence.startTimestamp = 1507665886;
+        discordPresence.endTimestamp = 1507665886;
+        discordPresence.largeImageText = "Numbani";
+        discordPresence.smallImageText = "Rogue - Level 100";
+        discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+        discordPresence.partySize = 1;
+        discordPresence.partyMax = 5;
+        discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
+        discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+        Discord_UpdatePresence(&discordPresence);
+    }
          }
      }
 });
